f704ff3dfdefaa492a58ed2de11fbe36af44b84e
peters <p1100i.dev@gmail.com>
2016-07-27 21:44:36 +0200

    project elements are generated dynamically
    - does some cosmetics also

--- src/js/client/controller/about-controller.js
+++ src/js/client/controller/about-controller.js
@@ -1,7 +1,59 @@
 define(['app'], function (app) {
   app.controller('aboutController', ['$rootScope', '$scope', function AboutControllerFactory($rootScope, $scope) {
     var
+      PROJECTS = [
+        [
+          'jsorted',
+          'Visual and audio demo of some sorting methods',
+          'https://p1100i.github.io/jsorted',
+          'bar-chart'
+        ],
+
+        [
+          'generator-morf',
+          'A yeoman generator to bootstrap a finely tuned [node, angular] project',
+          'https://www.github.com/p1100i/generator-morf',
+          'space-shuttle'
+        ],
+
+        [
+          'quadtree2.js',
+          '2d spatial indexing structure',
+          'https://p1100i.github.io/quadtree2.js',
+          'map'
+        ],
+
+        [
+          'click-da-game-of-life.js',
+          'JS implementation of Conways - Game Of Life',
+          'https://p1100i.github.io/click-da-game-of-life.js',
+          'th'
+        ]
+      ],
+
+      createProject = function createProject(name, description, href, icon) {
+        return {
+          'name'        : name,
+          'description' : description,
+          'href'        : href,
+          'icon'        : icon
+        };
+      },
+
+      getProjects = function getProjects() {
+        var
+          i,
+          projects = [];
+
+        for (i = 0; i < PROJECTS.length; i++) {
+          projects.push(createProject.apply(null, PROJECTS[i]));
+        }
+
+        return projects;
+      },
+
       init = function init() {
+        $scope.projects = getProjects();
       };
 
     init();
